83a034424f44d41fee41c50941a38a1547c39a50
Jordyn Newnham <jordyn@husk.pro>
2019-08-19 11:24:10 +1000

    Massive refactor. Clean up entire project workspace and group packages together to not clutter space.

--- src/main/java/conj/Shop/tools/DoubleUtil.java
+++
@@ -1,9 +0,0 @@
-package conj.Shop.tools;
-
-import java.text.NumberFormat;
-
-public class DoubleUtil {
-    public static String toString(final Double value) {
-        return NumberFormat.getInstance().format(value);
-    }
-}
---
+++ src/main/java/conj/shop/commands/ConfigManagement.java
@@ -0,0 +1,47 @@
+package conj.shop.commands;
+
+import conj.shop.commands.control.Control;
+import conj.shop.commands.control.Manager;
+import conj.shop.data.enums.Config;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class ConfigManagement {
+    public void run(final Player player, final Command cmd, final String label, final String[] args) {
+        if (args.length < 2) {
+            if (!Manager.getAvailableCommands(player, "config").isEmpty()) {
+                player.sendMessage(ChatColor.GRAY + "/shop config help");
+            } else {
+                player.sendMessage(Config.PERMISSION_ERROR.toString());
+            }
+            return;
+        }
+        final String command = args[1];
+        if (command.equalsIgnoreCase("edit")) {
+            if (!player.hasPermission("shop.config.edit")) {
+                player.sendMessage(Config.PERMISSION_ERROR.toString());
+            }
+        } else {
+            if (!command.equalsIgnoreCase("help")) {
+                return;
+            }
+            final List<String> help = Manager.getAvailableCommands(player, "config");
+            if (help.isEmpty()) {
+                player.sendMessage(Config.PERMISSION_ERROR.toString());
+                return;
+            }
+            int index = 1;
+            if (args.length == 3) {
+                try {
+                    index = Integer.parseInt(args[2]);
+                } catch (NumberFormatException ex) {
+                }
+            }
+            final String header = ChatColor.GRAY + "  === " + ChatColor.DARK_GREEN + "shop Config Help" + ChatColor.GRAY + " === " + ChatColor.DARK_GREEN + "Page " + ChatColor.GREEN + "%index%" + ChatColor.GRAY + "/" + ChatColor.GREEN + "%size%" + ChatColor.GRAY + " ===";
+            Control.list(player, help, index, header, 7);
+        }
+    }
+}
